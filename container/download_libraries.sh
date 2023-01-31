6f96f0afc2441fb5e9b10eb563ee5351398cde40
Mattia Rubini <mattia.rubini@gmail.com>
2023-01-31 19:37:28 +0100

    Added Dockerfile

---
+++ container/download_libraries.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# Dependencies
+wget github.com/PCRE2Project/pcre2/releases/download/pcre2-10.40/pcre2-10.40.tar.gz -O pcre2.tar.gz
+wget http://zlib.net/zlib-1.2.13.tar.gz -O zlib.tar.gz
+wget http://www.openssl.org/source/openssl-1.1.1p.tar.gz -O openssl.tar.gz
+# Nginx
+wget https://nginx.org/download/nginx-1.23.2.tar.gz -O nginx.tar.gz
